ab06a0045ec3050ae619a1435d1f338c9a98aa93
Etienne Audouin <etienne.audouin-ext@pole-emploi.fr>
2021-11-14 22:22:41 +0100

    feat: ajoute la possibilite de choisir les couleurs

--- axidraw.group-by-color.py
+++ axidraw.group-by-color.py
@@ -4,14 +4,15 @@ import argparse
 import xml.etree.ElementTree as ElementTree
 
 from collections import defaultdict
-from colors import get_html_color_name_from_hex
+from colors import get_html_color_name_from_hex, COLORS
 from colors import html_color_to_hex
 
 
-def get_color_dict(svg_file, model):
+def get_color_dict(svg_file, model, colors):
     """
     Creates a dictionary mapping based on available hex_colors in the svg file.
     Colors are processed on the stroke attribute or the fill attribute.
+    :param colors: the available colors to process
     :param svg_file: the file to process
     :param model: "stroke" or "fill"
     :return: a dictionary mapping a color name to a svg child element.
@@ -19,12 +20,13 @@ def get_color_dict(svg_file, model):
     tree = ElementTree.parse(svg_file)
     root = tree.getroot()
     dictionary = defaultdict(list)
-    return next_step(root, model, dictionary)
+    return next_step(root, model, dictionary, colors)
 
 
-def next_step(node, model, dictionary):
+def next_step(node, model, dictionary, colors):
     """
     Do the `get_color_dict` recursion
+    :param colors:  the available colors to process
     :param dictionary: the dictionary mapping a color name to a svg child element.
     :param node: current node in the tree
     :param model: "stroke" or "fill"
@@ -32,10 +34,10 @@ def next_step(node, model, dictionary):
     """
     for child in node:
         if child.tag == '{http://www.w3.org/2000/svg}g':
-            next_step(child, model, dictionary)
+            next_step(child, model, dictionary, colors)
         else:
             if model in child.attrib:
-                color = get_html_color_name_from_hex(child.attrib[model])
+                color = get_html_color_name_from_hex(child.attrib[model], colors)
                 child.attrib[model] = html_color_to_hex(color)
                 dictionary[color].append(child)
     return dictionary
@@ -76,6 +78,7 @@ def parse_args():
     parser.add_argument('svg_file', help='svg file path')
     parser.add_argument('--model', default='stroke', help='stroke or fill')
     parser.add_argument('--output', default='out.svg', help='output file')
+    parser.add_argument('--colors', nargs='+', default=None, help='colors to apply')
     return parser.parse_args()
 
 
@@ -84,7 +87,18 @@ def main():
     Gets the job done
     """
     args = parse_args()
-    dictionary = get_color_dict(args.svg_file, args.model)
+    svg_file = args.svg_file
+    model = args.model
+    colors = args.colors
+    if colors is None:
+        colors = COLORS
+    else:
+        buffer = {}
+        for color in colors:
+            buffer[color] = COLORS[color]
+        colors = buffer
+
+    dictionary = get_color_dict(svg_file, model, colors)
     root = render_svg(dictionary)
     save_svg(root, args.output)
 
